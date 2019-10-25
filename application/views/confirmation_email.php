8bd99ce1e28136b524ec6d6bf14963798dbae0b3
ainaajidian <ainaajidian@gmail.com>
2019-10-25 09:37:58 +0700

    Update view email confirmation

--- application/views/confirmation_email.php
+++ application/views/confirmation_email.php
@@ -52,10 +52,12 @@
             display: block;
             margin: 13px 0;
         }
+
+
     </style>
     <!--[if !mso]><!-->
     <style type="text/css">
-        @media only screen and (max-width:480px) {
+        @media only screen and (max-width:600px) {
             @-ms-viewport {
                 width: 320px;
             }
@@ -86,49 +88,82 @@
     <div class="mj-container" style="background-color:#FFFFFF;">
         <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0" width="600" align="center" style="width:600px;">        <tr>          <td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;">      <![endif]-->
         <div style="margin:0px auto;max-width:600px;">
-            <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0">
-                <tbody>
-                    <tr>
-                        <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:9px 0px 9px 0px;">
-                            <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:600px;">      <![endif]-->
-                            <div class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
-                                <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
-                                    <tbody>
-                                        <tr>
-                                            <td style="word-wrap:break-word;font-size:0px;padding:15px 15px 15px 15px;" align="center">
-                                                <div style="cursor:auto;color:#000000;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:11px;line-height:1.5;text-align:center;">
-                                                    <h3 style="line-height: 100%;">Thanks for using Member LF, click button below to activating your account &#xA0;</h3>
-                                                    <p></p>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                        <tr>
-                                            <td style="word-wrap:break-word;font-size:0px;padding:20px 20px 20px 20px;" align="center">
-                                                <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;" align="center" border="0">
-                                                    <tbody>
-                                                        <tr>
-                                                            <td style="border:0px solid #000;border-radius:24px;color:#fff;cursor:auto;padding:12px 36px;" align="center" valign="middle" bgcolor="#8B6F4E">
-                                                                <a href="<?php echo $url;?>" style="text-decoration:none;background:#8B6F4E;color:#fff;font-family:Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif;font-size:18px;font-weight:normal;line-height:120%;text-transform:none;margin:0px;" target="_blank">Activate My Account !!</a></td>
-                                                        </tr>
-                                                    </tbody>
-                                                </table>
-                                            </td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </div>
-                            <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <br><br>
-            Jika ada kendala pada Aplikasi Member LF, segera menghubungi Kami.<br>
-            Terima kasih atas kepercayaan anda menggunakan Aplikasi Member LF. <br>
-            <p class='footer'> Terima Kasih, <br>
-            Developer Aplikasi Member LF
-        </div>
+        <table role="presentation" cellpadding="0" cellspacing="0" style="font-size:0px;width:100%;" align="center" border="0">
+            <tbody>
+                <tr>
+                    <td style="text-align:center;vertical-align:top;direction:ltr;font-size:0px;padding:9px 0px 9px 0px;">
+                        <!--[if mso | IE]>      <table role="presentation" border="0" cellpadding="0" cellspacing="0">        <tr>          <td style="vertical-align:top;width:600px;">      <![endif]-->
+                        <div class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                        <span class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                        <span class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                        <span class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+                        <span class="mj-column-per-100 outlook-group-fix" style="vertical-align:top;display:inline-block;direction:ltr;font-size:13px;text-align:left;width:100%;">
+
+                        <!-- Start Logo LF top -->
+                        <center><a href="https://rpgroup.co.id/new-arrival-lf/"><img src="<?=base_url();?>assets/app_assets/lf_logo.png" width="162" height="162"></a></center></span></span></span></span>
+                        <!-- End Logo LF top -->
+                        <table role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                            <tbody>
+
+                                <!-- Start Text Thanks -->
+                                <tr>
+                                    <td style="word-wrap:break-word;font-size:0px;padding:15px 15px 15px 15px;" align="center">
+                                        <div style="cursor:auto;color:#000000;font-family:Ubuntu, Helvetica, Arial, sans-serif;font-size:14px;line-height:1.5;text-align:center;">
+                                            <h3 style="line-height: 100%;">Thank you for using Member LF,<br>
+                                               click button below to activating your account:&#xA0;</h3>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <!-- End Text Thanks -->
+
+                                <!-- Start Button Active Account -->
+                                <tr>
+                                    <td style="word-wrap:break-word;font-size:0px;padding:0px 20px 20px 20px;" align="center">
+                                        <table role="presentation" cellpadding="0" cellspacing="0" style="border-collapse:separate;" align="center" border="0">
+                                            <tbody>
+                                                <tr>
+                                                    <td style="border:0px solid #000;border-radius:24px;color:#fff;cursor:auto;padding:5px 36px;" align="center" valign="middle" bgcolor="#8B6F4E">
+                                                        <a href="<?php echo $url;?>" style="text-decoration:none;background:#8B6F4E;color:#fff;font-family:Ubuntu, Helvetica, Arial, sans-serif, Helvetica, Arial, sans-serif;font-size:18px;font-weight:normal;line-height:120%;text-transform:none;margin:0px;" target="_blank">Activate My Account</a>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                                <!-- End Button Active Account -->
+
+                                <!-- -->
+                                <tr>
+                                    <td style="word-wrap:break-word;font-size:0px;padding:5px 20px 5px 20px;" align="center">
+                                        <table role="presentation" style="width:600px; align-content: center;">
+                                            <tbody>
+                                                <tr>
+                                                    <th style="font-size:12px; text-align:center; padding-top: 5px; padding-bottom:5px;">FIND US ON:</th>
+                                                    <th style="font-size:12px; text-align:center; padding-top: 5px; padding-bottom:5px;">FOLLOW US ON:</th>
+                                                </tr>
+                                                <tr>
+                                                    <td style="width: 300px; padding-bottom:5px;">
+                                                        <center><a href="#"><img src="<?=base_url();?>assets/app_assets/ios.png" alt="" style="width:60px; height:35px; margin-right:2.5px;"></a> 
+                                                        <a href="https://play.google.com/store/apps/details?id=com.tukangaplikasi.member_lf"><img src="<?=base_url();?>assets/app_assets/android.png" alt="" style="width:65px; height:35px; margin-left:2.5px;"></a></center>
+                                                    </td>
+                                                    <td style="width: 300px; padding-bottom:5px;">
+                                                        <center><a href="https://www.instagram.com/lesfemmes.id/"><img src="<?=base_url();?>assets/app_assets/ig_logo.png" style="width:36px; height:36px;" alt=""></a> </center>
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </tbody>
+                          </table>
+</div>
+                        <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+                    </td>
+                </tr>
+            </tbody>
+        </table>
         <!--[if mso | IE]>      </td></tr></table>      <![endif]-->
+        </div>
     </div>
 </body>
 
